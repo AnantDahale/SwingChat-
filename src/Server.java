0d3f0df2ea41761ba4a3574c67e09b62e61efabc
Anant Dahale <anantdahale@gmail.com>
2025-11-17 19:28:03 +0530

    initial commit

---
+++ src/Server.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import javax.swing.*;
+public class Server extends JFrame{
+
+    Server(){
+
+        
+
+
+        setSize(450, 700);
+        setLocation(200, 50);
+        getContentPane().setBackground(Color.white);
+         
+        
+        
+        setVisible(true);
+
+    }
+    public static void main(String[] args) throws Exception {
+        new Server();
+    }
+}
